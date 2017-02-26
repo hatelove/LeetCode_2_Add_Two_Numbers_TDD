d425e68c74c800a2de4fe50e03542ed68a21b24e
hatelove <jay.hatelove@gmail.com>
2017-02-26 16:10:01 +0800

    initialized and add the first failed test case: L1 is 5, L2 is 4, should return 9

---
+++ LeetCode_2_Add_Two_Numbers_TDD/UnitTest1.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode_2_Add_Two_Numbers_TDD
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void L1_is_5_and_L2_is_4_should_return_9()
+        {
+            var l1 = new ListNode(5);
+            var l2 = new ListNode(4);
+            var expected = new ListNode(9);
+
+            Assert.AreEqual(expected.val, new Solution().AddTwoNumbers(l1, l2).val);
+        }
+    }
+}
