e07fea53eab0912145346182048ae2ed43c7f8dc
hatelove <jay.hatelove@gmail.com>
2017-02-26 16:11:13 +0800

    pass the first test case: return new ListNode(L1.val+L2.val)

---
+++ LeetCode_2_Add_Two_Numbers_TDD/Solution.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace LeetCode_2_Add_Two_Numbers_TDD
+{
+    /**
+     * Definition for singly-linked list.
+     * public class ListNode {
+     *     public int val;
+     *     public ListNode next;
+     *     public ListNode(int x) { val = x; }
+     * }
+     */
+
+    public class Solution
+    {
+        public ListNode AddTwoNumbers(ListNode l1, ListNode l2)
+        {
+            return new ListNode(l1.val + l2.val);
+        }
+    }
+
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+
+        public ListNode(int x)
+        {
+            val = x;
+        }
+    }
+}
